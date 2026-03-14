e95ffc6e1466a840677cc4610dced4e60d82b4ea
Juan Pablo Caballero <juan.ccastellanos@mail.escuelaing.edu.co>
2026-03-13 23:03:01 -0500

    feat: Configuración CORS

--- src/main/java/com/clubfight/LAMENTACIONES_USER_MANAGEMENT/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/clubfight/LAMENTACIONES_USER_MANAGEMENT/infrastructure/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/v1/users/**").permitAll()
-                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .anyRequest().authenticated()
             )
             .sessionManagement(session -> session
