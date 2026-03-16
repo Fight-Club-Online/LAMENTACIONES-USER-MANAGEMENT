7dd0378b095f732558829d1e0bbb5cfbab7a8c93
Juan Pablo Caballero <juan.ccastellanos@mail.escuelaing.edu.co>
2026-03-15 21:37:38 -0500

    bugfix: corrección CORS

--- src/main/java/com/clubfight/LAMENTACIONES_USER_MANAGEMENT/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/clubfight/LAMENTACIONES_USER_MANAGEMENT/infrastructure/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.clubfight.LAMENTACIONES_USER_MANAGEMENT.infrastructure.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,7 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
- * Configuración para la seguridad de la API.
+ * Configuración de seguridad de la API.
  */
 @Configuration
 public class SecurityConfig {
@@ -25,8 +26,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http
+            .cors(Customizer.withDefaults()) 
+            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/oauth/**").permitAll() 
                 .requestMatchers("/api/v1/users/**").permitAll()
                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .anyRequest().authenticated()
