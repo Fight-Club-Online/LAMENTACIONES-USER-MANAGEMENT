6b825a33196dc206e6afa5894b0212521ce6f675
Juan Pablo Caballero <juan.ccastellanos@mail.escuelaing.edu.co>
2026-03-15 20:38:14 -0500

    bugfix: corrección redis v3

--- src/main/java/com/clubfight/LAMENTACIONES_USER_MANAGEMENT/infrastructure/config/RedisConfig.java
+++ src/main/java/com/clubfight/LAMENTACIONES_USER_MANAGEMENT/infrastructure/config/RedisConfig.java
@@ -1,20 +1,41 @@
 package com.clubfight.LAMENTACIONES_USER_MANAGEMENT.infrastructure.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 /**
- * Configuración para redis.
+ * Configuración redis
  */
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
 
-    @Bean 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
+
+    @Value("${spring.data.redis.ssl.enabled:false}")
+    private boolean redisSsl;
+
+    @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration(redisHost, redisPort);
+        serverConfig.setPassword(redisPassword);
+
+        LettuceClientConfiguration clientConfig = redisSsl ? 
+            LettuceClientConfiguration.builder().useSsl().build() : 
+            LettuceClientConfiguration.builder().build();
+
+        return new LettuceConnectionFactory(serverConfig, clientConfig);
     }
-    
 }
